d82b06d58943c7fea7f82e3203a973352b7d0859
Arnav Sharma <arnavsharma2711@gmail.com>
2023-03-01 17:40:53 +0530

    Fix

--- index.js
+++ index.js
@@ -14,7 +14,9 @@ const app = express();
 app.use(cors());
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
 
