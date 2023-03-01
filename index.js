3c38c0b7895899a9714a7595361ca346f7f52bc7
Arnav Sharma <arnavsharma2711@gmail.com>
2023-03-01 17:43:11 +0530

    Fix

--- index.js
+++ index.js
@@ -15,12 +15,6 @@ app.use(cors({
   origin: 'http://localhost:3000'
 }));
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
 
 // Configure body-parser middleware to handle JSON data
 app.use(bodyParser.json());
