a692acfba337c61b7406a796578c87b8476ad9d9
Arnav Sharma <arnavsharma2711@gmail.com>
2023-03-02 14:25:07 +0530

    Final

--- index.js
+++ index.js
@@ -14,8 +14,7 @@ const app = express();
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', '*');
-  res.header('Access-Control-Allow-Methods', '*');
-  
+  res.header('Access-Control-Allow-Methods', '*');  
   next();
 });
 
